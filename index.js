b58baf5140f71af04a7a75daa24ae5a69e730cfb
Neal <nealrossov@gmail.com>
2024-09-04 15:41:55 +0300

    1

---
+++ index.js
@@ -0,0 +1,22 @@
+
+
+// let value = "";
+
+// if (value == "Коршунов") {
+//     window.location.href = '/1button.html';
+// }  
+
+
+// // else if (value3 == "Аналитик" ) {
+// //   document.getElementById("myForm").action = "./2button.html";
+// // }  else if (value1 < "31.12.2015") {
+// //   document.getElementById("myForm").action = "/3button.html";
+// // } else {
+// //   document.getElementById("myForm").action = "/index.html";
+// // } 
+
+// let num = 10;
+
+// if (num > 0) {
+//   console.log("The number is positive");
+// }
